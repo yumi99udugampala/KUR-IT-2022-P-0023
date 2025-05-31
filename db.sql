ed742a7c132823212b648983540408492233ce52
yumi99udugampala <prasadini99.udugampala@gmail.com>
2025-05-31 12:26:28 +0530

    login app

---
+++ db.sql
@@ -0,0 +1,5 @@
+create database myProject;
+use myProject;
+create table theUser(id int primary key auto_increment,uname varchar(50),upswrd varchar(50),uemail varchar(50), umobile varchar(20));
+desc theUser;
+select * from theUser;
